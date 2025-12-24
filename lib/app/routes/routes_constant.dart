cee4436bc86160d0be4f48c23501750b00c2d6da
xSupsatien <supsatien@hotmail.com>
2025-12-24 21:13:34 +0700

    refactor: cleanup routes constants

--- lib/app/routes/routes_constant.dart
+++ lib/app/routes/routes_constant.dart
@@ -1,10 +1,9 @@
-final String splash = '/';
-final String dashboard = '/dashboard';
-final String login = '/login';
-
-
-
+/// Route names constants for the application
 class RoutesName {
+  // Private constructor to prevent instantiation
+  RoutesName._();
+
+  // Route names
   static const String splashScreen = 'splash_screen';
   static const String dashboardScreen = 'dashboard_screen';
   static const String loginScreen = 'login_screen';
